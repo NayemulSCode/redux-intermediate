2f36ed87fdbc63d50ff57cb157a2335b09c12268
Nayemul <nayemulhasan2016@gmail.com>
2021-05-26 03:07:14 +0600

    payload

--- src/components/BookShopA.js
+++ src/components/BookShopA.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import { buyBook } from '../redux'
 
 function BookShopA() {
+    const [number, setNumber] = useState(1)
     const numberOfBooks = useSelector(state => state.book.numberOfBooks)
     const dispatch = useDispatch()
     return (
         <div>
             <h2>Number of books:{numberOfBooks} </h2>
-            <button onClick={()=> dispatch(buyBook())} >Buy Books</button>
+            <input type="text" value={number} onChange ={e => setNumber(e.target.value)} />
+            <button onClick={()=> dispatch(buyBook(number))} >Buy {number} Books</button>
         </div>
     )
 }
